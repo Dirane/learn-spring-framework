c3a6227e4bb4e94337ba4b37657ec14b4442d974
Dirane <diranengala@gmail.com>
2024-03-05 19:18:58 +0100

    Added multible ways of getting and establishing communication between Beans

--- src/main/java/com/in28miutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28miutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28miutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age){ };
+record Person(String name, int age, Address address){ };
 record Address(String firstLine, String city){ };
 @Configuration
 public class HelloWorldConfiguration {
@@ -19,11 +19,25 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person(){
-    return new Person("Ngala", 20);
+    return new Person("Ngala", 20, new Address("Bomaka", "Buea"));
     }
 
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameter(String name, int age, Address address3){
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
     public Address address(){
-    return new Address("67686768", "Buea");
+    return new Address("Busumbu", "Limbe");
+    }
+    @Bean(name = "address3")
+    public Address address3(){
+        return new Address("Mayor Street", "Buea");
     }
 }
