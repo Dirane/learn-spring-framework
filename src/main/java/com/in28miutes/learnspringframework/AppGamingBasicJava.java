62c299d67d196195d47a04053139bc9c30f4eb4f
Dirane <diranengala@gmail.com>
2024-03-05 14:35:53 +0100

    Added the PacManGame class

--- src/main/java/com/in28miutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28miutes/learnspringframework/AppGamingBasicJava.java
@@ -1,10 +1,11 @@
 package com.in28miutes.learnspringframework;
 
 public class AppGamingBasicJava {
-    public static void main(String[] ars)
+    public static void main(String[] args)
     {
-    var game = new MarioGame();
+//    var game = new MarioGame();
 //    var game = new SuperContraGame();
+    var game = new PacManGame();
     var gameRunner = new GameRunner(game);
     gameRunner.run();
     }
